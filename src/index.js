6d36a12fa0ce2dae0b5adecc40b43af304b9807a
bartosz.antoniak@onet.pl <bartosz.antoniak@onet.pl>
2022-10-25 20:55:32 +0200

    gh-pages

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './css/calendar-days-event.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Register/>
+    <Login/>
     <Calendar />
 
   </React.StrictMode>
