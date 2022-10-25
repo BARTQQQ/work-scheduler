b3dc405e6b47d8ad6f00b5aad294a4addc6005ef
bartosz.antoniak@onet.pl <bartosz.antoniak@onet.pl>
2022-10-25 20:45:59 +0200

    deploy

---
+++ build/precache-manifest.d295c4c0b828fba760c04cc6103b3d54.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb203c6be37c472c931d",
+    "url": "/work-scheduler/static/css/main.39544dd3.chunk.css"
+  },
+  {
+    "revision": "eb203c6be37c472c931d",
+    "url": "/work-scheduler/static/js/main.eb203c6b.chunk.js"
+  },
+  {
+    "revision": "f454ac9bedec4e31d7c6",
+    "url": "/work-scheduler/static/js/1.f454ac9b.chunk.js"
+  },
+  {
+    "revision": "92e3cf60ac6cc8864b2b",
+    "url": "/work-scheduler/static/js/runtime~main.92e3cf60.js"
+  },
+  {
+    "revision": "951cd60f004ff0ccd416bf7a7fc6634d",
+    "url": "/work-scheduler/static/media/edit-pen.951cd60f.svg"
+  },
+  {
+    "revision": "0b5e1ff52253d6f35dd52256988e6e3d",
+    "url": "/work-scheduler/static/media/plus.0b5e1ff5.svg"
+  },
+  {
+    "revision": "72fc595ac472c94db8f1cd6b871bf40b",
+    "url": "/work-scheduler/static/media/trash.72fc595a.svg"
+  },
+  {
+    "revision": "e029281ccb50cf0020086756d10288e8",
+    "url": "/work-scheduler/static/media/arrow-left.e029281c.svg"
+  },
+  {
+    "revision": "ee845e63eb2d80b3d109f7f70d1c995b",
+    "url": "/work-scheduler/static/media/arrow-right.ee845e63.svg"
+  },
+  {
+    "revision": "3c8b61d73fa000e8f11955f98530cbaf",
+    "url": "/work-scheduler/index.html"
+  }
+];
