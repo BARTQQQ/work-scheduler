1ce8a75ec3f18e4b7856fa4be94fd1b64f2e2204
bartosz.antoniak@onet.pl <bartosz.antoniak@onet.pl>
2023-01-17 23:12:21 +0100

    api cors

--- api/index.js
+++ api/index.js
@@ -21,13 +21,11 @@ connect()
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  next();
-});
+app.use(cors({
+    origin: 'https://work-scheduler-bartqqq.vercel.app',
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 
 app.use('/api/user', require('./routes/user.route'))
 app.use('/api/group', require('./routes/group.route'))
